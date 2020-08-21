45fa7ad8a6dbca17af9ebfde9a9e2aa5c9d4ed22
AndreeaLakatos <andreealakatos17@yahoo.com>
2020-08-21 14:49:19 +0300

    + Added base project

---
+++ android/app/src/main/kotlin/com/example/horus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.horus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Horus',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    );
+  }
+}
